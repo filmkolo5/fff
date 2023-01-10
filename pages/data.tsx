84042458173d4e7baa53cc5063826950701ea820
chalita janpan <chalita_0910@hotmail.com>
2023-01-10 13:25:37 +0700

    แก้ไข้DATA0

--- pages/data.tsx
+++ pages/data.tsx
@@ -9,25 +9,28 @@ export default function
     <div>
       <header>
         <div className='logo_ihub' >
-          <img src="/img/1.png" alt='' height={48}/>
+          <img src="/img/1.png" alt='' height={50}/>
           <a>PEA  IHub </a>
           <div className='menu'>
           <li><a href='#'><FiUsers /> ข้อมูลบุคลากร</a></li>
           <li><a href='#'><CgFileDocument/> หลักสูตร </a></li>
           <li><a href='#'><TbDatabase/> งบประมาณ</a></li>
-          <div className='login_1'>
-          <a href='#'> Login</a>
-          </div>
+          <div className='login-ihub'>
+          <li><a href='#'> Login</a></li>
           </div>
         </div>
+        </div>
       </header>
+
+
         <div className='data'>
           <h3>ข้อมูลบุคลากร</h3>
           </div>
           <div className='dataTable'>
             <h6>ตารางข้อมูลพนักงาน</h6>
-          <div className='search'></div>
-          <div className='searchInput'></div>
+          <div className='bginputsearch'>
+          <input type=" ID"name=" ID"placeholder="EMPLOYEE ID" />
+          </div>
           <div className='searchIcon'></div>
 
           </div>
