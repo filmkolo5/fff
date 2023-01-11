183f1b997e44bdb4d970219b5d0025e447625512
Taksinx <zxcasd1175@hotmail.com>
2023-01-11 16:06:22 +0700

    UPDATE DATA WITH NINI LEARNING

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,34 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import"../styles/index.css";
-import"../styles/data.css";
+import "../styles/index.css";
+import "../styles/data.css";
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div>
+    <header>
+      <div className='logo_ihub' >
+        <div className='left-menu'>
+          <div className='start-menu'>
+            <img src="/img/1.png" alt='' height={50} />
+            <a className='logo-header'>PEA  IHub </a>
+          </div>
+          <div className='menu'>
+            <li><a href='#'> ข้อมูลบุคลากร</a></li>
+            <li><a href='#'> หลักสูตร </a></li>
+            <li><a href='#'> งบประมาณ</a></li>
+          </div>
+        </div>
+
+        <div className='login-ihub'>
+          <li><a href='#'> Login</a></li>
+        </div>
+      </div>
+    </header>
+    <div className='layout'>
+    <Component {...pageProps} />
+    </div>
+    
+  </div>
+
+  // router.pathname == "/login"
+
 }
--- pages/data.tsx
+++ pages/data.tsx
@@ -2,12 +2,12 @@ import React, { useEffect } from 'react'
 import { FiUsers } from "react-icons/fi";
 import { CgFileDocument } from "react-icons/cg";
 import { TbDatabase } from "react-icons/Tb";
-import { BsSearch} from "react-icons/Bs";
 import { FiEdit } from "react-icons/fi";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import { useState } from 'react';
 import usersJson from "../json/data.json";
-
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 interface IUsers {
   "number": number;
   "employee_id": number;
@@ -21,170 +21,85 @@ interface IUsers {
 export default function () {
   const [users, setUsers] = useState<IUsers[]>([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     setUsers([...usersJson.users]);
   }, [])
 
   return (
     <div>
-      <header>
+      {/* <header>
         <div className='logo_ihub' >
-          <img src="/img/1.png" alt='' height={50}/>
+          <img src="/img/1.png" alt='' height={50} />
           <a>PEA  IHub </a>
           <div className='menu'>
-          <li><a href='#'><FiUsers /> ข้อมูลบุคลากร</a></li>
-          <li><a href='#'><CgFileDocument/> หลักสูตร </a></li>
-          <li><a href='#'><TbDatabase/> งบประมาณ</a></li>
-          <div className='login-ihub'>
-          <li><a href='#'> Login</a></li>
+            <li><a href='#'><FiUsers /> ข้อมูลบุคลากร</a></li>
+            <li><a href='#'><CgFileDocument /> หลักสูตร </a></li>
+            <li><a href='#'><TbDatabase /> งบประมาณ</a></li>
+            <div className='login-ihub'>
+              <li><a href='#'> Login</a></li>
+            </div>
           </div>
         </div>
-        </div>
-      </header>
+      </header> */}
 
 
-        <div>
-          <h3>ข้อมูลบุคลากร</h3>
-          <div className='dataTable'>
-            <h6>ตารางข้อมูลพนักงาน</h6>
+      {/* <div>
+        <h3>ข้อมูลบุคลากร</h3>
+        <div className='dataTable'>
+          <h6>ตารางข้อมูลพนักงาน</h6>
           <div className='bginputsearch'>
-          <input type=" ssearch"name=" ssearch"placeholder="Search " />
-          <div className='searchIcon'><BsSearch/>
-          <button className='add_data'> + ข้อมูลพนักงาน </button>
-          
+            <input type=" ssearch" name=" ssearch" placeholder="Search " />
+            <div className='searchIcon'>
+              <button className='add_data'> + ข้อมูลพนักงาน </button>
+
+            </div>
+
           </div>
-          
-          </div>
-          
-          </div> 
-          
-          </div> 
-<table className='MAT_TABLE'>
-<tbody>
-  <tr>
-    <th className='HEAD_BOX_NUMBER'>ลำดับ</th>
-    <th className='HEAD_BOX_EMPLOYEE_ID'>รหัสพนักงาน</th>
-    <th className='HEAD_BOX_FNAME_LNAME'>ชื่อ - นามสกุล</th>
-    <th className='HEAD_BOX_POSITION'>ตำแหน่งย่อ</th>  
-    <th className='HEAD_BOX_AFFILIATION'>สังกัด</th>  
-    <th className='HEAD_BOX_IHUB_POSITION'>ต่ำแหน่ง IHub</th>  
-    <th className='HEAD_BOX_PERFORMANCE'>การปฏิบัติงาน</th>
-    <td className='HEAD_BOX_DELETE_EDIT'></td>
-  </tr>
-  {users.map((user) => {
-    return (<tr key={user.employee_id}>
-      <td className='BOX_NUMBER'>{user.number}</td>
-      <td className='BOX_EMPLOYEE_ID'>{user.employee_id}</td>
-      <td className='BOX_FNAME_LNAME'>{user.fname_lname}</td>
-      <td className='BOX_POSITION'>{user.position}</td>
-      <td className='BOX_AFFILIATION'>{user.affiliation}</td>
-      <td className='BOX_IHUB_POSITION'>{user.ihub_position}</td>
-      <td className='BOX_PERFORMANCE'>None</td>
-      <td className='BOX_DELETE_EDIT'><FiEdit size="1rem"/>   <RiDeleteBin6Line size="1rem"/></td>
-    </tr>);
-  })}
-  {/* <tr>
-    <td className='BOX_NUMBER'>1.</td>
-    <td className='BOX_EMPLOYEE_ID'>409490</td>
-    <td className='BOX_FNAME_LNAME'>นายจักรพันธ์ อร่ามดิลกรัตน์</td>
-    <td className='BOX_POSITION'>วศก.4</td>
-    <td className='BOX_AFFILIATION'>ผอฟ.1 กอฟ. ฝสฟ. รผก.(วศ)</td>
-    <td className='BOX_IHUB_POSITION'>บุคลากรที่เกี่ยวเนื่องกับโครงการฯ</td>
-    <td className='BOX_PERFORMANCE'>None</td>
-    <td className='BOX_DELETE_EDIT'><FiEdit size="1rem"/>   <RiDeleteBin6Line size="1rem"/></td>
-  </tr>
-  <tr>
-    <td className='BOX_NUMBER'>2.</td>
-    <td className='BOX_EMPLOYEE_ID'>409490</td>
-    <td className='BOX_FNAME_LNAME'>นายจักรพันธ์ อร่ามดิลกรัตน์</td>
-    <td className='BOX_POSITION'>วศก.4</td>
-    <td className='BOX_AFFILIATION'>ผอฟ.1 กอฟ. ฝสฟ. รผก.(วศ)</td>
-    <td className='BOX_IHUB_POSITION'>บุคลากรที่เกี่ยวเนื่องกับโครงการฯ</td>
-    <td className='BOX_PERFORMANCE'>None</td>
-    <td className='BOX_DELETE_EDIT'><FiEdit size="1rem"/>   <RiDeleteBin6Line size="1rem"/></td>
-  </tr>
-  <tr>
-    <td className='BOX_NUMBER'>3.</td>
-    <td className='BOX_EMPLOYEE_ID'>409490</td>
-    <td className='BOX_FNAME_LNAME'>นายจักรพันธ์ อร่ามดิลกรัตน์</td>
-    <td className='BOX_POSITION'>วศก.4</td>
-    <td className='BOX_AFFILIATION'>ผอฟ.1 กอฟ. ฝสฟ. รผก.(วศ)</td>
-    <td className='BOX_IHUB_POSITION'>บุคลากรที่เกี่ยวเนื่องกับโครงการฯ</td>
-    <td className='BOX_PERFORMANCE'>None</td>
-    <td className='BOX_DELETE_EDIT'><FiEdit size="1rem"/>   <RiDeleteBin6Line size="1rem"/></td>
-  </tr>
-  <tr>
-   <td className='BOX_NUMBER'>4.</td>
-    <td className='BOX_EMPLOYEE_ID'>409490</td>
-    <td className='BOX_FNAME_LNAME'>นายจักรพันธ์ อร่ามดิลกรัตน์</td>
-    <td className='BOX_POSITION'>วศก.4</td>
-    <td className='BOX_AFFILIATION'>ผอฟ.1 กอฟ. ฝสฟ. รผก.(วศ)</td>
-    <td className='BOX_IHUB_POSITION'>บุคลากรที่เกี่ยวเนื่องกับโครงการฯ</td>
-    <td className='BOX_PERFORMANCE'>None</td>
-    <td className='BOX_DELETE_EDIT'><FiEdit size="1rem"/>   <RiDeleteBin6Line size="1rem"/></td>
-  </tr>
-  <tr>
-    <td className='BOX_NUMBER'>5.</td>
-    <td className='BOX_EMPLOYEE_ID'>409490</td>
-    <td className='BOX_FNAME_LNAME'>นายจักรพันธ์ อร่ามดิลกรัตน์</td>
-    <td className='BOX_POSITION'>วศก.4</td>
-    <td className='BOX_AFFILIATION'>ผอฟ.1 กอฟ. ฝสฟ. รผก.(วศ)</td>
-    <td className='BOX_IHUB_POSITION'>บุคลากรที่เกี่ยวเนื่องกับโครงการฯ</td>
-    <td className='BOX_PERFORMANCE'>None</td>
-    <td className='BOX_DELETE_EDIT'><FiEdit size="1rem"/>   <RiDeleteBin6Line size="1rem"/></td>
-  </tr>
-  <tr>
-   <td className='BOX_NUMBER'>6.</td>
-    <td className='BOX_EMPLOYEE_ID'>409490</td>
-    <td className='BOX_FNAME_LNAME'>นายจักรพันธ์ อร่ามดิลกรัตน์</td>
-    <td className='BOX_POSITION'>วศก.4</td>
-    <td className='BOX_AFFILIATION'>ผอฟ.1 กอฟ. ฝสฟ. รผก.(วศ)</td>
-    <td className='BOX_IHUB_POSITION'>บุคลากรที่เกี่ยวเนื่องกับโครงการฯ</td>
-    <td className='BOX_PERFORMANCE'>None</td>
-    <td className='BOX_DELETE_EDIT'><FiEdit size="1rem"/>   <RiDeleteBin6Line size="1rem"/></td>
-  </tr>
-  <tr>
-    <td className='BOX_NUMBER'>7.</td>
-    <td className='BOX_EMPLOYEE_ID'>409490</td>
-    <td className='BOX_FNAME_LNAME'>นายจักรพันธ์ อร่ามดิลกรัตน์</td>
-    <td className='BOX_POSITION'>วศก.4</td>
-    <td className='BOX_AFFILIATION'>ผอฟ.1 กอฟ. ฝสฟ. รผก.(วศ)</td>
-    <td className='BOX_IHUB_POSITION'>บุคลากรที่เกี่ยวเนื่องกับโครงการฯ</td>
-    <td className='BOX_PERFORMANCE'>None</td>
-    <td className='BOX_DELETE_EDIT'><FiEdit size="1rem"/>   <RiDeleteBin6Line size="1rem"/></td>
-  </tr>
-  <tr>
-    <td className='BOX_NUMBER'>8.</td>
-    <td className='BOX_EMPLOYEE_ID'>409490</td>
-    <td className='BOX_FNAME_LNAME'>นายจักรพันธ์ อร่ามดิลกรัตน์</td>
-    <td className='BOX_POSITION'>วศก.4</td>
-    <td className='BOX_AFFILIATION'>ผอฟ.1 กอฟ. ฝสฟ. รผก.(วศ)</td>
-    <td className='BOX_IHUB_POSITION'>บุคลากรที่เกี่ยวเนื่องกับโครงการฯ</td>
-    <td className='BOX_PERFORMANCE'>None</td>
-    <td className='BOX_DELETE_EDIT'><FiEdit size="1rem"/>   <RiDeleteBin6Line size="1rem"/></td>
-  </tr>
-  <tr>
-    <td className='BOX_NUMBER'>9.</td>
-    <td className='BOX_EMPLOYEE_ID'>409490</td>
-    <td className='BOX_FNAME_LNAME'>นายจักรพันธ์ อร่ามดิลกรัตน์</td>
-    <td className='BOX_POSITION'>วศก.4</td>
-    <td className='BOX_AFFILIATION'>ผอฟ.1 กอฟ. ฝสฟ. รผก.(วศ)</td>
-    <td className='BOX_IHUB_POSITION'>บุคลากรที่เกี่ยวเนื่องกับโครงการฯ</td>
-    <td className='BOX_PERFORMANCE'>None</td>
-    <td className='BOX_DELETE_EDIT'><FiEdit size="1rem"/>   <RiDeleteBin6Line size="1rem"/></td>
-  </tr>
-  <tr>
-    <td className='BOX_NUMBER'>10.</td>
-    <td className='BOX_EMPLOYEE_ID'>409490</td>
-    <td className='BOX_FNAME_LNAME'>นายจักรพันธ์ อร่ามดิลกรัตน์</td>
-    <td className='BOX_POSITION'>วศก.4</td>
-    <td className='BOX_AFFILIATION'>ผอฟ.1 กอฟ. ฝสฟ. รผก.(วศ)</td>
-    <td className='BOX_IHUB_POSITION'>บุคลากรที่เกี่ยวเนื่องกับโครงการฯ</td>
-    <td className='BOX_PERFORMANCE'>None</td>
-    <td className='BOX_DELETE_EDIT'><FiEdit size="1rem"/>   <RiDeleteBin6Line size="1rem"/></td>
-  </tr> */}
-  </tbody>
-</table>
-</div>
+
+        </div>
+
+      </div> */}
+
+      <table className='MAT_TABLE'>
+        <tbody>
+          <tr>
+            <th className='HEAD_BOX_NUMBER'>ลำดับ</th>
+            <th className='HEAD_BOX_EMPLOYEE_ID'>รหัสพนักงาน</th>
+            <th className='HEAD_BOX_FNAME_LNAME'>ชื่อ - นามสกุล</th>
+            <th className='HEAD_BOX_POSITION'>ตำแหน่งย่อ</th>
+            <th className='HEAD_BOX_AFFILIATION'>สังกัด</th>
+            <th className='HEAD_BOX_IHUB_POSITION'>ต่ำแหน่ง IHub</th>
+            <th className='HEAD_BOX_PERFORMANCE'>การปฏิบัติงาน</th>
+            <td className='HEAD_BOX_DELETE_EDIT'></td>
+          </tr>
+          {users.map((user) => {
+            return (<tr key={user.employee_id}>
+              <td className='BOX_NUMBER'>{user.number}</td>
+              <td className='BOX_EMPLOYEE_ID'>{user.employee_id}</td>
+              <td className='BOX_FNAME_LNAME'>{user.fname_lname}</td>
+              <td className='BOX_POSITION'>{user.position}</td>
+              <td className='BOX_AFFILIATION'>{user.affiliation}</td>
+              <td className='BOX_IHUB_POSITION'>{user.ihub_position}</td>
+              <td className='BOX_PERFORMANCE'>None</td>
+              <td className='BOX_DELETE_EDIT'><FiEdit size="1rem" />   <RiDeleteBin6Line size="1rem" /></td>
+            </tr>);
+          })}
+   
+        </tbody>
+      </table>
+      <div>
+        {/* <DataTable value={users}
+          dataKey="id" paginator rows={10} rowsPerPageOptions={[5, 10, 25]}
+          paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+          currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products"
+          responsiveLayout="scroll">
+          <Column selectionMode="multiple" headerStyle={{ width: '3rem' }} exportable={false}></Column>
+          <Column field="number" header="number" sortable style={{ minWidth: '12rem' }}></Column>
+          <Column field="employee_id" header="employee_id" sortable style={{ minWidth: '16rem' }}></Column>
+          <Column field="image" header="Image" body={imageBodyTemplate}></Column>
+          <Column body={actionBodyTemplate} exportable={false} style={{ minWidth: '8rem' }}></Column>
+        </DataTable> */}
+      </div>
+    </div>
   )
 }
