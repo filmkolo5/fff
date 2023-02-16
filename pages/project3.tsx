2cac7cff2d8146dd2b16dd6911515a2c44a36826
Taksinx <zxcasd1175@hotmail.com>
2023-02-16 23:19:28 +0700

    update

--- pages/project3.tsx
+++ pages/project3.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect,useState } from 'react'
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 /* PRIME REACT */
@@ -8,7 +8,39 @@ import "primeicons/primeicons.css"
 import "primeflex/primeflex.css"
 /* Components */
 import { template2 } from '../components/template2';
+import axios from 'axios';
+
+
 export default function () {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    // ดึงค่าเริ่มต้นของ Projects มาแสดงในตาราง
+    fetchProjects();
+  }, []);
+
+  const fetchProjects = async () => {
+    const config = {
+      method: 'GET',
+      url: 'http://localhost:8080/project/all',
+    };
+    axios(config)
+    .then((response:any)=>{
+      console.log('fetch success : ',response.data);
+
+      const newProjects = response.data.map(( project:any , index:any ) => {
+        return ({
+          ...project,
+          'number':index+1,
+        })
+      })
+      setProjects(newProjects);
+    })
+    .catch((error:any)=>{
+      console.log('fetch error : ',error);
+    })
+  }
+
   return (
     <div>
       <div className='HEAD-PROJECT3'>
@@ -22,14 +54,14 @@ export default function () {
         <div className='grid'>
         <div className='col'>
 
-            <DataTable   paginator paginatorTemplate={template2}  first={0} rows={10}  paginatorClassName="justify-content-end" responsiveLayout="scroll" className='shadow'>
+            <DataTable value={[...projects]}  paginator paginatorTemplate={template2}  first={0} rows={10}  paginatorClassName="justify-content-end" responsiveLayout="scroll" className='shadow'>
             <Column  header='ลำดับ' field='number' className='ui-column-data' />
-            <Column  header='โครงการ' field='userName' className='ui-column-data' />
-            <Column  header='GEN/BATCH' field='fname_lname' className='ui-column-data' />
-            <Column  header='วันเริ่มต้น' field='position' className='ui-column-data' />
-            <Column  header='วันสิ้นสุด' field='affiliation' className='ui-column-data' />
-            <Column  header='จำนวนผู้เข้าร่วมทั้งหมด (คน)' field='positionIhub' className='ui-column-data' />
-            <Column  header='จำนวนผู้เข้าร่วมปัจจุบัน (คน)' field='positionIhub' className='ui-column-data' />
+            <Column  header='โครงการ' field='mainProject' className='ui-column-data' />
+            <Column  header='GEN/BATCH' field='genBatch' className='ui-column-data' />
+            <Column  header='วันเริ่มต้น' field='startProjectAt' className='ui-column-data' />
+            <Column  header='วันสิ้นสุด' field='endProjectAt' className='ui-column-data' />
+            <Column  header='จำนวนผู้เข้าร่วมทั้งหมด (คน)' field='totalEmployee' className='ui-column-data' />
+            <Column  header='จำนวนผู้เข้าร่วมปัจจุบัน (คน)' field='nowEmployee' className='ui-column-data' />
             <Column header='Action' field='positionIhub' className='ui-column-data' />
           </DataTable>
         </div>
